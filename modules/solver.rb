97e5e09a944e13907732e7bfd0c5c7a6e0944a2d
Lancelot-SO <fsowah001@gmail.com>
2023-04-21 07:57:23 +0000

    add solver file

--- modules/solver.rb
+++ modules/solver.rb
@@ -1,4 +1,11 @@
 class Solver
+  def factorial(number)
+    raise ArgumentError, 'Number must be postitve' if number.negative?
 
-  
+    number.zero? ? 1 : number * factorial(number - 1)
+  end
+
+  def reverse(word)
+    word.reverse
+  end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,10 +1,21 @@
 required '../modules/solver'
 
 describe Solver do
-
-
-  
-  
+  before :each do
+    @solver = Solver.new
+  end
+  it 'should return instance of Solver' do
+    expect(@solver).to be_instance_of(Solver)
+  end
+  it 'should return fatorial of 5 to be 120' do
+    expect(@solver.factorial(5)).to eq(120)
+  end
+  it 'should raise argumental  error if a number is negative ' do
+    expect { @solver.factorial(-1) }.to raise_error(ArgumentError)
+  end
+  it "should return reverse word of 'hello' to be 'olleh'" do
+    expect(@solver.reverse('hello')).to eq('olleh')
+  end
   
   context 'Implementing fizzbuzz method in Solver'
     it "returns 'fizz' when the input is divisible by 3" do
